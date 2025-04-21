5ed270af6e2f732625663bc52bd7b3ea73612727
Jay5000-bhatt <bhattjay114@gmail.com>
2025-04-21 19:19:36 +0530

    updated fullly upgraded

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import ConnectDB from "../Config/ConnectDB.js";
-import authRoutes from "../Routes/authRoutes.js";
-import postRoutes from "../Routes/postRoutes.js";
+import ConnectDB from "./Config/ConnectDB.js";
+import authRoutes from "./Routes/authRoutes.js";
+import postRoutes from "./Routes/postRoutes.js";
 
 dotenv.config();
 ConnectDB();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
 app.use(
   cors({
     origin: "https://postify-nine.vercel.app",
@@ -19,13 +21,14 @@ app.use(
   })
 );
 
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-
 app.get("/", (req, res) => {
   res.send("My Server is Running Successfully 🚀");
 });
 
-export default app;
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
