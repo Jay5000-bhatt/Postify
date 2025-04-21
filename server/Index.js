c73808e40081ef55fbb660279bc277d3cff58178
Jay5000-bhatt <bhattjay114@gmail.com>
2025-04-21 18:46:49 +0530

    Finally sorted

--- server/Index.js
+++ server/Index.js
@@ -14,13 +14,11 @@ ConnectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const corsOptions = {
-//   origin: 'https://postify-nine.vercel.app/',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   allowedHeaders: 'Content-Type,Authorization',
-// };
-
-app.use(cors());
+app.use(cors({
+  origin: 'https://postify-nine.vercel.app/', 
+  methods: 'GET,POST,PUT,DELETE',          
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
